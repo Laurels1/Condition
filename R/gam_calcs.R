dee4ccc550cc55e2a24a3245548f9b23cfb5fdb7
laurels1 <Laurelcol@hotmail.com>
2019-09-20 23:34:04 -0400

    Removed outliers, exported GAM summary stats to a csv

--- R/gam_calcs.R
+++ R/gam_calcs.R
@@ -19,24 +19,24 @@ out.dir = "output"
 #Removed 4 outliers where Butterfish STOM_VOLUME >10,
 #Removed 1 outlier where spotted hake EXPCATCHNUM >5000
 ##Need to figure out how to not drop NAs
-CondClean <- stom.epu %>% dplyr::filter(!(is.na(STOM_VOLUME) | Species == "American Plaice" & stom_full >0.3),
-                                        !(is.na(STOM_VOLUME) | Species == "Butterfish" & STOM_VOLUME >10),
-                                        !(is.na(EXPCATCHNUM) | Species == "Spotted Hake" & EXPCATCHNUM >5000))
+CondClean <- stom.epu %>% dplyr::filter((is.na(stom_full) | !(Species == "American Plaice" & stom_full >0.3)),
+                                        (is.na(STOM_VOLUME) | !(Species == "Butterfish" & STOM_VOLUME >10)),
+                                        (is.na(EXPCATCHNUM) | !(Species == "Spotted Hake" & EXPCATCHNUM >5000)))
 
 
-   #GAM analyses relating condition to environmental parameters, by species:
+#GAM analyses relating condition to environmental parameters, by species:
 
 spp <- unique(stom.epu$Species)
 datalist = list()
 
-#spp <- spp[1]
 for(sp in spp) {
 condSPP <- stom.epu %>% dplyr::filter(Species==sp)
   
-condSPP <- stom.epu %>% dplyr::filter(SVSPP==13)
+#turn on for testing a single species outside of loop:
+#condSPP <- stom.epu %>% dplyr::filter(SVSPP==13)
 
    form.cond <- formula(RelCond ~ s(BOTTEMP, k=10) +s(EXPCATCHNUM, k=10) +s(LON, LAT, k=25) +s(stom_full, k=10), data=condSPP)
-                        #+s(EPU)  + s(stom_full))
+                        #+s(EPU)
   
    condGAM <- mgcv::gam(form.cond, family= gaussian, data=condSPP, select=T)
   
@@ -44,18 +44,15 @@ condSPP <- stom.epu %>% dplyr::filter(SVSPP==13)
 #                                              "EXPCATCHNUM" =~1+EXPCATCHNUM+s(EXPCATCHNUM),
 #                                              "LON, LAT" =~1+LON,LAT +s(LON,LAT),
 #                                              "YEAR" =~1+YEAR))
-  
-## Can't add output dir to sink
-#sink(paste0(out.dir/sp,"_condition.txt"))
-#print(summary(condGAM))
-#sink()
 
 GAMstats <- summary(condGAM)
 
-SumCondGAM <- c(Species = sp, p.coeff = GAMstats$s.pv, r.sp = GAMstats$r.sq, dev.expl = GAMstats$dev.expl, gcv = GAMstats$sp.criterion)
-SumCondGAM$sp <- sp
-datalist[[sp]] <- SumCondGAM
+SumCondGAM <- t(c(sp, round(GAMstats$s.pv,3),  round(GAMstats$r.sq,3), round(GAMstats$dev.expl,3),  round(GAMstats$sp.criterion,3)))
 
+dl=data.frame(SumCondGAM)
+GAMnames=c('Species', 'bt.p', 'num.p', 'lon.lat.p', 'stom.p', 'r.sq', 'dev.expl', 'gcv')
+names(dl)=GAMnames
+datalist[[sp]] <- dl
 
    filename <- here::here(out.dir,paste0(sp,"_condition.jpg"))
    jpeg(filename)
@@ -73,13 +70,3 @@ AllSPP = do.call(rbind, datalist)
 
 readr::write_csv(AllSPP, here::here(out.dir,"GAM_Condition_Summary.csv"))   
 
-#   stepgam <- gam::step.Gam(condGAM,scope=list(
-#     "BOTTEMP" = ~ 1 + BOTTEMP + lo(BOTTEMP) + S(BOTTEMP)),
-#     trace =T)
-  
-#   summary (stepgam)
-#   gamrun1.se <-plot (stepgam, ylab="Relative Condition", rug=T, cex=1.05, lwd=6, se=T)
-#   gamrun1.res <-plot (stepgam, ylab="Relative Condition", rug=T, cex=1.05, lwd=6,residuals=T)
-
-  
-#}
