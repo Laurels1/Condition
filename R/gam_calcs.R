2d3bae06d45040e37c56d7e7fb1e3b1aa6c98fe2
laurels1 <Laurelcol@hotmail.com>
2019-09-25 16:45:13 -0400

    Added relative condition aggregations by EPU (not enough data) and strata (just right. for now...)

--- R/gam_calcs.R
+++ R/gam_calcs.R
@@ -16,10 +16,24 @@ library(readr)
 out.dir = "output"
 data.dir <- "data"
 
-#Creating average Relative Condition and stomach fullness by tow, species, sex
-AvgTowCond <- stom.epu %>% group_by(CRUISE6, STRATUM, STATION, TOW, Species, sex) %>% 
-  mutate(AvgTowRelCond=(mean(RelCond)), AvgTowStomFull=(mean(stom_full))) %>%
-  distinct(AvgTowRelCond, .keep_all = T)
+#-------------------------------------------------------------
+#Explore different ways of aggregating Relative Condition:
+#Creating Average Relative Condition and Average Stomach Fullness by tow, species, sex
+#AvgTowCond <- stom.epu %>% group_by(CRUISE6, STRATUM, STATION, TOW, Species, sex) %>% 
+#  mutate(AvgTowRelCond=(mean(RelCond)), AvgTowStomFull=(mean(stom_full))) %>%
+#  distinct(AvgTowRelCond, .keep_all = T)
+
+#Creating Average Relative Condition and Average Stomach Fullness by strata, species, sex
+AvgStrataCond <- stom.epu %>% group_by(CRUISE6, STRATUM, Species, sex) %>% 
+  mutate(AvgRelCondStrata=(mean(RelCond)), AvgStomFullStrata=(mean(stom_full))) %>%
+  distinct(AvgRelCondStrata, .keep_all = T)
+
+#Creating Average Relative Condition and Average Stomach Fullness by EPU, species, sex
+#Couldn't run mechanisms model because data too sparse:
+#AvgEPUCond <- stom.epu %>% group_by(CRUISE6, EPU, Species, sex) %>% 
+#  mutate(AvgRelCondEPU=(mean(RelCond)), AvgStomFullEPU=(mean(stom_full))) %>%
+#  distinct(AvgRelCondEPU, .keep_all = T)
+#---------------------------------------------------------------
 
 #Bringing in average temperature data
 AvgTempSpringData <- readr::read_csv(here::here(data.dir, "AverageTempSpring.csv"))
@@ -41,8 +55,9 @@ AvgTempWinterFormat <- AvgTempWinterData %>% dplyr::rename(YEAR=Year) %>%
 
 AvgTemp <- Reduce(dplyr::full_join, list(AvgTempWinterFormat, AvgTempSpringFormat, AvgTempSummerFormat, AvgTempFallFormat))
 
-CondAvgTemp <- dplyr::left_join(AvgTowCond, AvgTemp, by=c("YEAR", "EPU"))
+CondAvgTemp <- dplyr::left_join(AvgStrataCond, AvgTemp, by=c("YEAR", "EPU"))
 
+#--------------------------------------------------------------------------------
 #Bringing in ratio of small to large copepods
 load("~/EDAB/Condition/Condition/data/1977_2017_SLI_Calfin_Pseudo_Ctyp.rdata")
 #View(Zooplankton_Primary_Prod)
@@ -56,9 +71,10 @@ CalfinFormat <- Calfin %>% dplyr::rename(YEAR = year) %>% select(YEAR, SLI.gbk, 
          if_else(CalEPU=='SLI.scs', 'SS', 'NA')))))
 
 CondCal <- dplyr::left_join(CondAvgTemp, CalfinFormat, by=c("YEAR", "EPU"))
-  
+ 
+#-------------------------------------------------------------------------------- 
 #Average stomach fullness by Species, YEAR, EPU and sex for the year before
-AvgStom <- CondCal %>% dplyr::group_by(Species, YEAR, EPU, sex) %>% dplyr::mutate(AvgStomFull=mean(stom_full, na.rm=TRUE))
+AvgStom <- CondCal %>% dplyr::group_by(Species, YEAR, EPU, sex) %>% dplyr::mutate(AvgStomFull=mean(AvgStomFullStrata, na.rm=TRUE))
 ##Can't get lag and mutate to work
 #AvgStomLag <- AvgStom %>% dplyr::mutate(AvgStomLag1=(AvgStomFull %in% YEAR-1))
 #AvgStomLag <- AvgStom %>% dplyr::lag(AvgStomLag1=(AvgStomFull, n=1)
@@ -72,15 +88,19 @@ Stomlag <- E %>% dplyr::mutate(YEAR = YEARstom+1)
 AvgStom2 <- AvgStom %>% dplyr::select(-c(AvgStomFull))
 AvgStomLag <- dplyr::left_join(AvgStom2, Stomlag, by=c("Species", "YEAR", "EPU", "sex"))
 
+#---------------------------------------------------------------------------------
 #Removed outlier where American Plaice stom_full >0.3, 
 #Removed 4 outliers where Butterfish STOM_VOLUME >10,
 #Removed 1 outlier where spotted hake EXPCATCHNUM >5000
-CondClean <- AvgStomLag %>% dplyr::filter((is.na(stom_full) | !(Species == "American Plaice" & stom_full >0.3)),
+CondClean <- AvgStomLag %>% dplyr::filter((is.na(AvgStomFullStrata) | !(Species == "American Plaice" & AvgStomFullStrata >0.3)),
                                         (is.na(STOM_VOLUME) | !(Species == "Butterfish" & STOM_VOLUME >10)),
                                         (is.na(EXPCATCHNUM) | !(Species == "Spotted Hake" & EXPCATCHNUM >5000)))
 
 
+#--------------------------------------------------------------------------------
 ####GAM analyses relating condition to environmental parameters, by species:
+#remove Sea Raven for LON/LAT runs since not enough data:
+#CondClean <- CondClean %>% dplyr::filter(Species!="Sea Raven")
 
 spp <- unique(CondClean$Species)
 datalist = list()
@@ -92,23 +112,24 @@ condSPP <- CondClean %>% dplyr::filter(Species==sp)
 #condSPP <- CondClean %>% dplyr::filter(Species=='Goosefish') %>% mutate(sp='Goosefish')
 
 #Full model
-#   form.cond <- formula(AvgTowRelCond ~ s(BOTTEMP, k=10) +s(EXPCATCHWT, k=10) +s(LON, LAT, k=25) +s(AvgStomFullLag, k=10) +s(CopepodSmallLarge) +s(AvgTempSpring) +s(YEAR), data=condSPP)
+#   form.cond <- formula(AvgRelCondStrata ~ s(BOTTEMP, k=10) +s(EXPCATCHWT, k=10) +s(LON, LAT, k=25) +s(AvgStomFullLag, k=10) +s(CopepodSmallLarge) +s(AvgTempSpring) +s(YEAR), data=condSPP)
 #Single index
-#  form.cond <- formula(AvgTowRelCond ~ s(BOTTEMP, k=10), data=condSPP)
-#  form.cond <- formula(AvgTowRelCond ~ s(EXPCATCHWT, k=10), data=condSPP)
-#  form.cond <- formula(AvgTowRelCond ~ s(EXPCATCHNUM, k=10), data=condSPP)
-#  form.cond <- formula(AvgTowRelCond ~ s(LON, LAT, k=25), data=condSPP)
-#  form.cond <- formula(AvgTowRelCond ~ s(AvgStomFullLag, k=10), data=condSPP)
-#  form.cond <- formula(AvgTowRelCond ~ s(stom_full, k=10), data=condSPP)
-  form.cond <- formula(AvgTowRelCond ~ s(CopepodSmallLarge, k=10), data=condSPP)
-  #form.cond <- formula(AvgTowRelCond ~ s(AvgTempSpring, k=10), data=condSPP)
-  #form.cond <- formula(AvgTowRelCond ~ s(AvgTempSummer, k=10), data=condSPP)
-  #form.cond <- formula(AvgTowRelCond ~ s(AvgTempFall, k=10), data=condSPP)
-  #form.cond <- formula(AvgTowRelCond ~ s(AvgTempWinter, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(BOTTEMP, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(EXPCATCHWT, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(EXPCATCHNUM, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(LON, LAT, k=25), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(AvgStomFullStrata, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(AvgStomFullLag, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(CopepodSmallLarge, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(AvgTempSpring, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(AvgTempSummer, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(AvgTempFall, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(AvgTempWinter, k=10), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(YEAR, k=10), data=condSPP)
 #Eplains highest deviance:
-  #form.cond <- formula(AvgTowRelCond ~ s(BOTTEMP, k=10) +s(EXPCATCHWT, k=10) +s(LON, LAT, k=25) +s(stom_full, k=10) +s(AvgStomFullLag, k=10) +s(CopepodSmallLarge) +s(AvgTempSpring), data=condSPP)
+#  form.cond <- formula(AvgRelCondStrata ~ s(BOTTEMP, k=10) +s(EXPCATCHWT, k=10) +s(LON, LAT, k=25) +s(AvgStomFullStrata, k=10) +s(AvgStomFullLag, k=10) +s(CopepodSmallLarge, k=10) +s(AvgTempSpring, k=10), data=condSPP)
 #Mechanisms model:
-  #form.cond <- formula(AvgTowRelCond ~ s(BOTTEMP, k=10) +s(EXPCATCHWT, k=10) +s(AvgStomFullLag, k=10) +s(CopepodSmallLarge) +s(AvgTempSpring), data=condSPP)
+  form.cond <- formula(AvgRelCondStrata ~ s(BOTTEMP, k=15) +s(EXPCATCHWT, k=15) +s(AvgStomFullLag, k=15) +s(CopepodSmallLarge, k=15) +s(AvgTempSpring, k=15), data=condSPP)
 
 
                         #Can add factor variable as a by variable: e.g. +s(LON, LAT, k=25, by = EPU)
@@ -127,8 +148,13 @@ SumCondGAM <- t(c(sp, round(GAMstats$s.pv,3),  round(GAMstats$r.sq,3), round(GAM
 
 dl=data.frame(SumCondGAM)
 #Full model output:
-#GAMnames=c('Species', 'Bottom Temp', 'Local Biomass', 'LON LAT', 'AvgStomFull', 'CopepodSL', 'AvgTempSpring', 'YEAR', 'R sq.', 'Deviance Explained', 'GCV', 'n')
-GAMnames=c('Species', 'CopepodSL', 'R sq.', 'Deviance Explained', 'GCV', 'n')
+#GAMnames=c('Species', 'Bottom Temp', 'Local Biomass', 'LON LAT', 'AvgStomFullLag', 'CopepodSL', 'AvgTempSpring', 'YEAR', 'R sq.', 'Deviance Explained', 'GCV', 'n')
+#Full Model with reasonable mechanisms relating to condition changes:
+GAMnames=c('Species', 'Bottom Temp', 'Local Biomass', 'AvgStomFullLag', 'CopepodSL', 'AvgTempSpring', 'R sq.', 'Deviance Explained', 'GCV', 'n')
+#Model with highest deviance explained:
+#GAMnames=c('Species', 'Bottom Temp', 'Local Biomass', 'LON LAT','Stomach fullness','AvgStomFullLag', 'CopepodSL', 'AvgTempSpring', 'R sq.', 'Deviance Explained', 'GCV', 'n')
+#single variable runs
+#GAMnames=c('Species', 'AvgStomFullStrata', 'R sq.', 'Deviance Explained', 'GCV', 'n')
 
 
 #error if you try to add YEAR to GAMnames because GAM doesn't include YEAR as a variable.
@@ -138,7 +164,7 @@ datalist[[sp]] <- dl
 #Use for testing plot with single species
 #filename <-here::here(out.dir, paste0('GoosefishYEAR_condition.jpg'))
 
-   filename <- here::here(out.dir,paste0(sp,"_CopepodSL_AvgCond.jpg"))
+   filename <- here::here(out.dir,paste0(sp,"_Mechanisms_AvgCondStrata.jpg"))
    jpeg(filename)
    par(mfrow=c(2,2), mar=c(2.15,2.15,0.15,0.25), mgp=c(0.25,1,0), cex=0.75, tck=-0.015)
    plot(condGAM, pages=1, residuals=TRUE, rug=T) #show partial residuals
@@ -147,11 +173,15 @@ datalist[[sp]] <- dl
    #plot(condGAM, pages=1, seWithMean=TRUE) #'with intercept' CIs
  
    
-### gam.check not working, erorr says could not find function "gam.check"
-#   gam.check(condGAM) # run model checks including checking smoothing basis dimensions
+#gam.check (run model checks including checking smoothing basis dimensions)
+   sink(here::here(out.dir,paste0(sp,"_GAMcheck_Mechanisms_AvgCondStrata.txt")))
+   
+   mgcv::gam.check(condGAM) 
+   
+   sink()
 }
 
 AllSPP = do.call(rbind, datalist)
 
-readr::write_csv(AllSPP, here::here(out.dir,"GAM_Summary_AvgRelCond_CopepodSL.csv"))   
+readr::write_csv(AllSPP, here::here(out.dir,"GAM_Summary_AvgRelCondStrata_Mechanisms.csv"))   
 
