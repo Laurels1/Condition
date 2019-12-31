29aaf0b74e0fad45c96d9f54c3e390b67b0724ec
laurels1 <Laurelcol@hotmail.com>
2019-12-31 14:47:57 -0500

    Updated condition plots with 2019 data

--- R/RunPlotCondition.R
+++ R/RunPlotCondition.R
@@ -1,6 +1,10 @@
 
 source("R/RelConditionEPU.R")
+condition <- RelConditionEPU(pullNewData=T,out.dir="output")
 
 condition <- RelConditionEPU(pullNewData = FALSE, out.dir="output")
 # plot results
-plot_condition(condition$condGOM, filename = "GOMcondition_2018", out.dir="output")
+plot_condition(condition$condGOM, filename = "GOMcondition_2019", out.dir="output")
+plot_condition(condition$condGB, filename = "GBcondition_2019", out.dir="output")
+plot_condition(condition$condMAB, filename = "MABcondition_2019", out.dir="output")
+plot_condition(condition$condSS, filename = "SScondition_2019", out.dir="output")
