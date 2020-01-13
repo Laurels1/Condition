f3c2f8788d88c23dbe491a5241579dbaa1e5638f
laurels1 <Laurelcol@hotmail.com>
2020-01-13 16:44:44 -0500

    Made condition plot red to blue, reduced margins

--- R/plot_condition.R
+++ R/plot_condition.R
@@ -69,23 +69,29 @@ plot_condition <- function(annualCondition,filename= "test1",out.dir = "output")
    colnames(conditionMatrix) <- as.vector(yearRange)
 
    # plot the matrix with specified color palette
-   jpeg(filename = here::here(out.dir,paste0(filename,".jpg")), res = 200, height = 2000, width = 1450)
-   graph.colors<-colorRampPalette(c('#C6E2FF','#00008B'))
-   par(mar = c(4, 2, 2, 11), fig = c(0, 1, 0.1, 1))
-   image(t(conditionMatrix),xaxt="n",yaxt="n",col=graph.colors(nQuants))
+#   jpeg(filename = here::here(out.dir,paste0(filename,".jpg")), res = 200, height = 2000, width = 1450)
+   jpeg(filename = here::here(out.dir,paste0(filename,".jpg")), res = 200, height = 1350, width = 1450)
+#  graph.colors<-colorRampPalette(c('#C6E2FF','#00008B')) #blue color palette
+  graph.colors<-colorRampPalette(c('steelblue2','tomato2')) #blue color palette
+#   graph.colors <- colorspace::diverge_hcl #Red lowest, blue highest
+#  graph.colors <- RColorBrewer::brewer.pal(5, "RdBu") #Divergent ramp red to blue
+#  par(mar = c(4, 2, 2, 11), fig = c(0, 1, 0.1, 1)) #fig indicates how much of plot to take up, with c(0,1,0,1) being the whole space
+   par(mar = c(1.4, 1.5, 1.5, 8), fig = c(0, 1, 0.15, 1))
+      image(t(conditionMatrix),xaxt="n",yaxt="n",col=graph.colors(nQuants))
   # xaxis
    axis(1,at=seq(0,1,length.out=length(yearRange)),labels=yearRange,cex.axis = 0.8,las=2)
    # yaxis (need to arrange in alphabetical)
    axis(4,at=seq(0,1,length.out=length(rowNames)),labels=rowNames,cex.axis=0.8,las=1)
   
    # legend
-   par(mar = c(4.5, 8, 0.0, 14), fig = c(0, 1, 0, 0.1), new = T)
+#   par(mar = c(4.5, 8, 0.0, 14), fig = c(0, 1, 0, 0.1), new = T)
+   par(mar = c(3, 8, 0.0, 14), fig = c(0, 1, 0, 0.1), new = T) # first number of mar is height of legend
 #   image(x=seq(0, 1, by = 1 / 4), z = t(matrix(1:4,1,4)), axes = F, col = graph.colors(4), xlim = c(0,1), ylim = c(0,1),xlab="")
 #   axis(1, at = seq(1/8, 1, by = 1 / 4), labels = c( 1, 2, 3, 4), cex = 2)
 #   axis(2,at = 0.5, labels="Quantile",las=2)
   #legend for 5 groupings:
    image(x=seq(0, 1, by = 1 / nQuants), z = t(matrix(1:nQuants,1,nQuants)), axes = F, col = graph.colors(nQuants), xlim = c(0,1), ylim = c(0,1),xlab="")
-   axis(1, at = seq(1/8, 1, by = 1 / nQuants), labels = c( "Very bad", "Bad", "Neutral", "Good", "Very Good"), cex = 2)
+   axis(1, at = seq(1/8, 1, by = 1 / nQuants), labels = c( "Poor Condition", "Bad", "Neutral", "Good", "Good Condition"), cex = 2)
    #axis(2,at = 0.5,las=2)
     box()
    dev.off()
