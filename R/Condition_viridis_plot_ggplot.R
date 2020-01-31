ad81aabc6ce7f4ad7db31c15d9175552528ecaa8
laurels1 <Laurelcol@hotmail.com>
2020-01-31 16:43:08 -0500

    Trying to plot condition using viridis

---
+++ R/Condition_viridis_plot_ggplot.R
@@ -0,0 +1,83 @@
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+library(forcats)
+library(scales)
+
+
+#annualCondition <- read.csv("https://raw.githubusercontent.com/Laurels1/Condition/master/data/AnnualRelCond2018_GB.csv",
+#                            stringsAsFactors = FALSE)
+
+out.dir="output"
+
+annualCondition <- condition$condMAB %>% 
+  dplyr::filter(!(EPU == "MAB" & YEAR == 2017))
+
+speciesNames <- annualCondition %>%
+  dplyr::filter(sex == "F") %>%
+  group_by(Species) %>% 
+  mutate(scaleCond = scale(MeanCond, scale = TRUE, center = TRUE))
+
+xs = quantile(speciesNames$scaleCond, seq(0,1, length.out = 6), na.rm = TRUE)
+#xs = quantile(speciesNames$scaleCond, seq(0,1, length.out = 5), na.rm = TRUE)
+CondBreaks = xs[2:6]
+
+
+speciesNames <- speciesNames %>% 
+  mutate(category = cut(scaleCond, breaks = xs
+                        , labels = c( "Poor Condition", 
+                                                            "Bad",
+                                                            "Neutral",
+                                                            "Good",
+                                                            "Good Condition")
+                        ))
+
+
+sortNames <- speciesNames %>% 
+  filter(YEAR > 2014) %>%
+  group_by(Species) %>% 
+  summarize(total = sum(scaleCond)) %>% 
+  arrange(desc(total)) %>% 
+  mutate(Species = factor(Species, levels = unique(Species))) %>%
+  pull(Species)
+
+speciesNames$Species <-   factor(speciesNames$Species, levels = sortNames)
+
+#See 5 scale colors for viridis:
+#scales::show_col(viridis::viridis_pal()(5))
+vir <- viridis::viridis_pal()(5)
+
+#jpeg("test.MABcondition_2019.jpg", res = 200, height = 1350, width = 1450)
+
+#p1 <-   ggplot(speciesNames, aes(x = YEAR, y = forcats::fct_rev(Species), fill = category)) +
+#  geom_tile() +
+#  coord_equal() +
+#  scale_fill_brewer(palette = "RdBu", na.value = "black") +
+#  theme_bw() +
+#  theme(legend.position="bottom")
+
+#ggsave("MABcondition_2019_test.jpg", width = 6, height = 4, units = "in", dpi = 300)
+
+ p2 <- ggplot(speciesNames, aes(x = YEAR, y = forcats::fct_rev(Species), fill = scaleCond)) +
+  geom_tile() +
+  coord_equal() +
+#  scale_fill_viridis_c()
+  scale_fill_viridis_c(breaks= CondBreaks, values= vir) +
+  theme_bw() +
+#  scale_fill_discrete(labels = c( "Poor Condition", 
+#                                  "Bad",
+#                                  "Neutral",
+#                                  "Good",
+#                                  "Good Condition")) +
+  scale_x_continuous(breaks=round(seq(min(1990), max(speciesNames$YEAR), by = 5))) +
+  theme(legend.position = "bottom", legend.title = element_blank(), 
+ #       legend.text = element_text(labels = c("-2" = "Poor Condition", 
+#                                               "0" = "Neutral",
+#                                               "3" = "Good Condition")),
+        axis.title = element_blank(), axis.text.x = element_text(size = 8),
+        axis.text.y = element_text(size = 8), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave("MABcondition_2019_viridis.jpg", width = 6, height = 3.75, units = "in", dpi = 300)
+
+#p1 + p2
---
+++ R/plot_condition_viridis.R
@@ -0,0 +1,130 @@
+#' Plots annual condition
+#' 
+#' Plots normalized condition factor (categorized into 4 quantiles)
+#' Quantiles used are 25%, 50%, 75% (This can be generalized at a later date)
+#' 
+#' @param annualCondition Tibble. (nx6). Columns = Species,EPU,sex,YEAR,MeanCond, nCond
+#' @param filename Character string. Name of output file (excluding file extendion)
+#' @param out.dir Character string. Name of directory where plots will be saved
+#' 
+#' @return data frame and exports a plot
+#' \item{conditionMatrix}{Values designates the quantile for each Species (row), in a given year (column)}
+#' 
+#' @section Plot:
+#' 
+#' The file format of the exported plot is jpg (size etc can be generalized at a later date)
+#' 
+#' @export
+
+
+plot_condition <- function(annualCondition,filename= "test1",out.dir = "output") {
+  #turn off if running in function:
+  annualCondition <- condMAB
+  
+  speciesNames <- annualCondition %>% dplyr::filter(sex == "F") %>%
+    dplyr::select(Species) %>%
+    dplyr::distinct(Species) %>% as.numeric(YEAR)
+  speciesNames <- speciesNames$Species
+  print(speciesNames)
+  
+  speciesNames <- as.numeric(YEAR)
+  
+  #Sort species names by highest to lowest condition averaged over the last 5 years
+  sortNames <- speciesNames %>% 
+    filter(YEAR == c("2015", "2016", "2017", "2018", "2019")) %>%
+    group_by(Species) %>% 
+    summarize(total = sum(scaleCond)) %>% 
+    arrange(desc(total)) %>% 
+    mutate(Species = factor(Species, levels = unique(Species))) %>%
+    pull(Species)
+  
+  speciesNames$Species <-   factor(speciesNames$Species, levels = sortNames)
+
+  # quantiles to group into
+#    quantilesNormal <- qnorm(c(0.25,0.5,0.75)) 
+  #5 groupings:
+     quantilesNormal <- qnorm(c(0.2,0.4,0.6,0.8)) 
+     nQuants <- length(quantilesNormal)+1
+  # values Q1 <= 0.25
+  # values 0.25 > Q2 <= 0.5
+  # values 0.5 > Q3 <= 0.75
+  # values 0.75 > Q4
+     
+#See 5 (or nQuants number of) scale colors for viridis:
+     #scales::show_col(viridis::viridis_pal()(5))
+     vir <- viridis::viridis_pal()(nQuants)
+     
+  #remove 2017 data if in MAB:
+  annualCondition <- annualCondition %>% dplyr::filter(!(EPU == "MAB" & YEAR == 2017))
+     
+  yearRange <- c(min(annualCondition$YEAR):max(annualCondition$YEAR))
+  # create an expanded grid to fill in missing years with NA's
+  fullTable <- dplyr::as_tibble(expand.grid(Species=speciesNames,YEAR= yearRange,sex = c("F"),stringsAsFactors = F))
+  annualCondition <- dplyr::right_join(annualCondition,fullTable,by=c("Species","YEAR","sex"))
+  
+  #remove 2017 data if in MAB:
+#  annualCondition <- annualCondition %>% dplyr::filter(!(EPU == "MAB" & YEAR == 2017))
+  
+  #This section below is for plotting annual condition:
+  conditionMatrix <- NULL# inital value
+  rowNames <- NULL # initial value 
+  for (species in speciesNames) { # each species is a row in the plot
+    for (asex in c("F")) {
+      # pull each species/Sex one at a time. 
+      
+      speciesData <- annualCondition %>% dplyr::filter(Species == species & sex == "F") %>% dplyr::arrange(YEAR)
+      if (dim(speciesData)[1] == 0) {next} # no data
+      # normalize data first
+      normData <- (speciesData$MeanCond - mean(speciesData$MeanCond,na.rm = T))/sd(speciesData$MeanCond,na.rm=T)
+      # each year is placed in category 1-4 based on quantiles above
+      quant <- 1
+      for (iq in quantilesNormal) {
+        quant <- quant + (normData > iq)
+      }
+      conditionMatrix <- rbind(conditionMatrix,quant)
+      rowNames <- rbind(rowNames,paste0(species))
+    }
+  }
+    # convert to dataframe and add row and column names
+   conditionMatrix <- as.data.frame(conditionMatrix)
+   rownames(conditionMatrix) <- as.vector(rowNames)
+   colnames(conditionMatrix) <- as.vector(yearRange)
+
+   # plot the matrix with specified color palette
+#   jpeg(filename = here::here(out.dir,paste0(filename,".jpg")), res = 200, height = 2000, width = 1450)
+   jpeg(filename = here::here(out.dir,paste0(filename,".jpg")), res = 200, height = 1350, width = 1450)
+#  graph.colors<-colorRampPalette(c('#C6E2FF','#00008B')) #blue color palette
+   #printer and color blind frindly color scales:
+#   install.packages("viridis")
+#   library(viridis)
+#   viridis_pal(option = "D")(5) 
+   graph.colors <- colorRampPalette(vir)
+#  graph.colors<-colorRampPalette(c('steelblue2','gray77','tomato2')) #blue, gray, red color palette
+#   graph.colors <- colorspace::diverge_hcl #Red lowest, blue highest
+#  graph.colors <- RColorBrewer::brewer.pal(5, "RdBu") #Divergent ramp red to blue
+#  par(mar = c(4, 2, 2, 11), fig = c(0, 1, 0.1, 1)) #fig indicates how much of plot to take up, with c(0,1,0,1) being the whole space
+   par(mar = c(1.4, 1.5, 1.5, 8), fig = c(0, 1, 0.15, 1))
+      image(t(conditionMatrix),xaxt="n",yaxt="n",col=graph.colors(nQuants))
+  # xaxis
+   axis(1,at=seq(0,1,length.out=length(yearRange)),labels=yearRange,cex.axis = 0.8,las=2)
+   # yaxis (need to arrange in alphabetical)
+   axis(4,at=seq(0,1,length.out=length(rowNames)),labels=rowNames,cex.axis=0.8,las=1)
+  
+   # legend
+#   par(mar = c(4.5, 8, 0.0, 14), fig = c(0, 1, 0, 0.1), new = T)
+   par(mar = c(3, 8, 0.0, 14), fig = c(0, 1, 0, 0.1), new = T) # first number of mar is height of legend
+#   image(x=seq(0, 1, by = 1 / 4), z = t(matrix(1:4,1,4)), axes = F, col = graph.colors(4), xlim = c(0,1), ylim = c(0,1),xlab="")
+#   axis(1, at = seq(1/8, 1, by = 1 / 4), labels = c( 1, 2, 3, 4), cex = 2)
+#   axis(2,at = 0.5, labels="Quantile",las=2)
+  #legend for 5 groupings:
+   image(x=seq(0, 1, by = 1 / nQuants), z = t(matrix(1:nQuants,1,nQuants)), axes = F, col = graph.colors(nQuants), xlim = c(0,1), ylim = c(0,1),xlab="")
+   axis(1, at = seq(1/8, 1, by = 1 / nQuants), labels = c( "Poor Condition", "Bad", "Neutral", "Good", "Good Condition"), cex = 2)
+   #axis(2,at = 0.5,las=2)
+    box()
+   dev.off()
+   
+   return(conditionMatrix)
+
+}
+  
+    
