de4c379f3b557c7630b20152ac8684bfb564baca
laurels1 <Laurelcol@hotmail.com>
2020-11-13 15:31:22 -0500

    Testing latest version of git

--- R/Condition_plot_viridis_final.R
+++ R/Condition_plot_viridis_final.R
@@ -9,7 +9,7 @@ out.dir="output"
 #Data from RelConditionEPU.R
 #No data available for 2020 due to Covid-19
 #Removed MAB values in 2017 due to low sampling coverage:
-annualCondition <- condGOM %>% 
+annualCondition <- condMAB %>% 
     dplyr::filter(!(EPU == "MAB" & YEAR == 2017)) 
 
 #change YEAR to continuous numeric for plotting function below:
@@ -51,13 +51,9 @@ p2 <- ggplot(speciesNames, aes(x = YEAR, y = forcats::fct_rev(Species), fill = c
     guides(fill = guide_legend(reverse = TRUE)) +
     #scale_x_discrete works if don't need to pad final year for missing data. Changed Year to numeric above and this works:
    scale_x_continuous(breaks=round(seq(min(1990), max(speciesNames$YEAR), by = 5))) +
-    #Pads final year of missing data but drops labels:
-      # scale_x_discrete(breaks=round(seq(min(1991), max(2020), by = 5)),
-      #                limits=c(1991:2020), labels=seq(min(1991), max(2020), by = 5)) +
         theme(legend.position = "right", legend.box = "vertical", legend.title = element_blank(), 
           axis.title = element_blank(), axis.text.x = element_text(size = 10),
           axis.text.y = element_text(size = 10), panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())
 
-#if scale_x_continuous, gives error: discrete value supplied for continuous variable. Changed to scale_x_discrete
-ggsave(path= here::here(out.dir),"GOMcondition_2020_viridis_final.jpg", width = 8, height = 3.75, units = "in", dpi = 300)
+ggsave(path= here::here(out.dir),"MABcondition_2020_viridis_final.jpg", width = 8, height = 3.75, units = "in", dpi = 300)
