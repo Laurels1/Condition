6c2f75e0640e0b9a353d9bbdb1e78b027f79ecc5
andybeet <22455149+andybeet@users.noreply.github.com>
2021-04-21 12:33:45 -0400

    add function to clean data

---
+++ fit/clean_data.R
@@ -0,0 +1,21 @@
+#' function to clean the data.
+#' 
+#' Remove explanatory variables that are not relevant to this species, have too
+#' many NAs. Remove rows that have NAs in any column since model can't deal 
+#' with missing values. Interpolate variable with missing data if necessary
+#'
+#'
+#'
+
+clean_data <- function(rawData) {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
