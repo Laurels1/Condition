96cb4f2aadf4ebbd833bff657832cc7f2cb86709
andybeet <22455149+andybeet@users.noreply.github.com>
2021-08-03 16:00:55 -0400

    added list of species with missing codes

--- R/map_SVSPP_ITIS.R
+++ R/map_SVSPP_ITIS.R
@@ -17,11 +17,27 @@ codes <- itis$data %>%
   dplyr::rename(SVSPP=SVSPPsv) %>%
   dplyr::distinct()
 
+# find species names with missing ITIS codes
+allSurveySpecies <- survdat::get_species(channel)$data
+speciesWithMissingITIS <- allSurveySpecies %>%
+  dplyr::filter(SCINAME %in% itis$missing$cfdbs.species_itis_ne) %>% 
+  dplyr::select(SCINAME,COMNAME,SVSPP)
 
-# COMNAME                 SCIENTIFIC_NAME SVSPPsv
-# 1  ROUGHTAIL STINGRAY              DASYATIS CENTROURA       4
-# 2  BLUNTNOSE STINGRAY                    DASYATIS SAY      18
-# 3        BULLNOSE RAY         MYLIOBATIS FREMINVILLEI      19
-# 4    LONGHORN SCULPIN MYOXOCEPHALUS OCTODECEMSPINOSUS     163
-# 5          OCEAN POUT         MACROZOARCES AMERICANUS     193
-# 6 SPINY BUTTERFLY RAY                GYMNURA ALTAVELA     375
+
+# These are the species
+# SCINAME                         COMNAME              SVSPP
+# <chr>                           <chr>                <dbl>
+#   1 TORPEDO NOBILIANA               ATLANTIC TORPEDO        21
+# 2 MYLIOBATIS FREMINVILLEI         BULLNOSE RAY            19
+# 3 MYOXOCEPHALUS OCTODECEMSPINOSUS LONGHORN SCULPIN       163
+# 4 MENTICIRRHUS AMERICANUS         SOUTHERN KINGFISH      652
+# 5 MENTICIRRHUS SAXATILIS          NORTHERN KINGFISH      146
+# 6 MACROZOARCES AMERICANUS         OCEAN POUT             193
+# 7 LEPOPHIDIUM PROFUNDORUM         FAWN CUSK-EEL          194
+# 8 GYMNURA ALTAVELA                SPINY BUTTERFLY RAY    375
+# 9 GYMNURA MICRURA                 SMOOTH BUTTERFLY RAY   376
+# 10 DASYATIS AMERICANA              SOUTHERN STINGRAY       29
+# 11 DASYATIS CENTROURA              ROUGHTAIL STINGRAY       4
+# 12 DASYATIS SAY                    BLUNTNOSE STINGRAY      18
+# 13 ACIPENSER OXYRHYNCHUS           ATLANTIC STURGEON      380
+# 14 ZENOPSIS CONCHIFERA             BUCKLER DORY           112
