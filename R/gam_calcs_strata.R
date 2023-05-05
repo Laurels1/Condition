2bdaa9e7a7c81256342298f86b9b467bd7bdd181
laurels1 <40471009+Laurels1@users.noreply.github.com>
2023-05-05 15:14:04 -0400

    Immature mackerel EDM data for Rob Gamble

--- R/gam_calcs_strata.R
+++ R/gam_calcs_strata.R
@@ -94,7 +94,7 @@ AvgStrataCond <- CondStockUnit %>% group_by(CRUISE6, STRATUM, Species, sex) %>%
          AvgLonStrata = (mean(LON)), AvgBottomTempStrata = (mean(BOTTEMP)), AvgSurfaceTempStrata = (mean(SURFTEMP))) %>%
   distinct(AvgRelCondStrata, .keep_all = T)
 
-#Creating Average Relative Condition by Year, species for shelf-wide regime shift work (Scott Large Dynamic Factor Analysis and Rob Gamble EDM)
+#Creating Average Relative Condition by Year, species for shelf-wide regime shift work (Scott Large Dynamic Factor Analysis)
 AvgYearCond <- CondStockUnit %>% group_by(YEAR, Species) %>%
   mutate(AvgRelCondYear=(mean(!is.na(RelCond))), AvgRelCondYearSD = (sd(!is.na(RelCond))), AvgExpcatchwtYear = (mean(!is.na(BIOMASS))),
          AvgExpcatchnumYear= (mean(!is.na(ABUNDANCE))), AvgLatYear = (mean(!is.na(LAT))),
@@ -227,7 +227,7 @@ SpringSplit4 <- SpringRegimeResults$index[4]
 # GLORYSnaStratOrder <- GLORYSnaStratum %>% arrange(STRATUM)
 
 #--------------------------------------------------------------------------------
-#Bringing in small/large copepods, total copepods and zooplankton abundance anomaly by strata from Harvey Walsh:
+#Bringing in small-large copepods, total copepods and zooplankton abundance anomaly by strata from Harvey Walsh:
 #Change NaN's to NA's before importing
 ZooplStrata <- readr::read_csv(here::here(data.dir,"EcoMon_ZooplanktonData2021_BTSMeanAbundance.csv"), col_names = T)
 
@@ -266,7 +266,7 @@ ZoopIndexStrata <- Reduce(dplyr::full_join, list(SmLgCop, TotCop, ZoopAbund))
 
 ZoopData <- dplyr::left_join(CondAvgTemp, ZoopIndexStrata, by=c('YEAR', 'STRATUM'))
 
-#Zooplankton data by EPU, YEAR for Scott Large Dynamic Factor Analysis and Rob Gamble EDM:
+#Zooplankton data by EPU, YEAR for Scott Large Dynamic Factor Analysis:
 ZoopDataEPU <- ZoopData %>% group_by(YEAR, EPU, SEASON) %>% 
   dplyr:: mutate(CopepodSmLgSpringEPU=(mean(CopepodSmallLargeStrataSpring, na.rm=TRUE)),
                  CopepodSmLgSummmerEPU=(mean(CopepodSmallLargeStrataSummer, na.rm=TRUE)),
@@ -311,6 +311,9 @@ CalfinFormat <- Calfin %>% dplyr::rename(YEAR = year) %>%
                                               if_else(CalEPU=='SLI.scs', 'SS', 'NA')))))
 
 CondCal <- dplyr::left_join(ZoopDataEPU, CalfinFormat, by=c("YEAR", "EPU"))
+
+#Small-large copepod index for Rob Gamble EDM:
+saveRDS(CalfinFormat,file = here::here("other",paste0("SmallLargeCopepods_EDM2022.rds")))
 
 #Test for regime shifts in Copepod small/large ratio (same method as in Perretti et al. 2017, although Perretti uses MRT, gives error when method="mrt"):
 CopepodEPU <- CalfinFormat %>% dplyr::filter(YEAR >= 1992) %>% dplyr::select(YEAR, CopepodSmallLarge)
@@ -1046,6 +1049,23 @@ EDMdata <- EDMdataTemp %>% unique() %>% dplyr::filter(YEAR >= 1992)
 #readr::write_csv(DFAdata, here::here(out.dir,"FishCondition_EnvirCov_DFA2022.csv"))
 saveRDS(EDMdata,file = here::here("other",paste0("FishCondition_EDM2022.rds")))
 
+#EDM for immature mackerel <=23cm:
+annualcondImm <- cond.epu  %>% dplyr::filter(Species == 'Atlantic mackerel', LENGTH <= 23, YEAR >= 1992)  %>%
+  dplyr::group_by(YEAR) %>% dplyr::summarize(ImmatureMackerelCond = mean(RelCond), ImmMackStdDevCond = sd(RelCond), nCond = dplyr::n())
+condN <- dplyr::filter(annualcond, nCond>=3) %>% ungroup()
+
+EDMzoopGOM <- ZoopDataEPU %>% dplyr::filter(EPU == 'GOM') %>%
+  ungroup() %>% dplyr::select('YEAR', 'ZoopAbundSummerEPU') %>% dplyr::rename(ZoopAbundSummerGOM = ZoopAbundSummerEPU)
+
+EDMdataZoopImm <- dplyr::full_join(annualcondImm, EDMzoopGOM, by='YEAR')
+
+EDMtempSpr <- AvgTemp %>% dplyr::select('YEAR', 'AvgTempSpring')
+
+EDMdataImm2 <- dplyr::full_join(EDMtempSpr, EDMdataZoopImm, by='YEAR')
+
+EDMdataImm <- EDMdataImm2 %>% unique() %>% dplyr::filter(YEAR >= 1992)
+
+saveRDS(EDMdataImm,file = here::here("other",paste0("ImmMackerel_FishCondition_EDM2022.rds")))
 
 #Attempting to select values less than -0.3 or greater than 0.3 but not working:
 # EnVarCorSig <- EnVarCor %>% filter(('AvgExpcatchwtStrata' < -0.3 | 'AvgExpcatchwtStrata' > 0.3) |
